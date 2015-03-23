ac24a12c242e384a42b6b137abff0adcd852e846
Arfaan <amajothi@hotmail.com>
2015-03-23 16:35:51 +0000

    Migrations

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -17,6 +17,14 @@ namespace AngularTutorial.Models
         {
         }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<TodoContext, Migrations.Configuration>());
+        }
+
+
+
         public DbSet<Todo> Todoes { get; set; }
     }
 }
