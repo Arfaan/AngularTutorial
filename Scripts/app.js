ec7d027e070f5d6f1b618487213fa5a9d24658f8
Arfaan <amajothi@hotmail.com>
2015-03-24 13:26:09 +0000

    Charts

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,14 @@
-﻿var TodoApp = angular.module("TodoApp", ["ngResource", "ngRoute"]).
+﻿
+
+
+var TodoApp = angular.module("TodoApp", ["ngResource", "ngRoute", 'googlechart']).
     config(function ($routeProvider) {
         $routeProvider.
             when('/', { controller: ListCtrl, templateUrl: 'list.html' }).
             when('/new', { controller: CreateCtrl, templateUrl: 'details.html' }).
             when('/edit/:editID', { controller: EditCtrl, templateUrl: 'details.html' }).
-            when('/graph', { controller:    GraphCtrl, templateUrl: 'graph.html' }).
+            when('/test', { controller: TestCtrl, templateUrl: 'test.html' }).
+          
             otherwise({ redirectTo: '/' });
     });
 
@@ -147,4 +151,211 @@ var ListCtrl = function ($scope, $location, Todo) {
 
     };
 
+var TestCtrl = function ($scope, $location, Todo) {
+    angular.module('TodoApp', ['googlechart'])
+
+    var chart1 = {};
+    chart1.type = "google.visualization.BarChart";
+    chart1.displayed = false;
+    chart1.data = {
+        "cols": [{
+            id: "month",
+            label: "Month",
+            type: "string"
+        }, {
+            id: "laptop-id",
+            label: "Laptop",
+            type: "number"
+        }, {
+            id: "desktop-id",
+            label: "Desktop",
+            type: "number"
+        }, {
+            id: "server-id",
+            label: "Server",
+            type: "number"
+        }, {
+            id: "cost-id",
+            label: "Shipping",
+            type: "number"
+        }],
+        "rows": [{
+            c: [{
+                v: "2014"
+            }, {
+                v: 19,
+                f: "42 items"
+            }, {
+                v: 12,
+                f: "Ony 12 items"
+            }, {
+                v: 7,
+                f: "7 servers"
+            }, {
+                v: 4
+            }]
+        }
+
+        , {
+            c: [{
+                v: "2013"
+            }, {
+                v: 13
+            }, {
+                v: 1,
+                f: "1 unit (Out of stock this month)"
+            }, {
+                v: 12
+            }, {
+                v: 2
+            }]
+        }
+        
+        , {
+            c: [{
+                v: "2012"
+            }, {
+                v: 24
+            }, {
+                v: 5
+            }, {
+                v: 11
+            }, {
+                v: 6
+            }
+
+           ]
+        }]
+    };
+
+    chart1.options = {
+        "title": "Performance",
+        "series": {
+            0: { color: '#004225' },
+            1: { color: 'green' },
+            2: { color: '#C80000' },
+            3: { color: '#E6B800' },
+            
+   
+        },
+
+        "fill": 20,
+        "displayExactValues": true,
+        "vAxis": {
+            "title": "Sales unit",
+            "gridlines": {
+                "count": 10
+            }
+        },
+        "hAxis": {
+            "title": "Date"
+        }
+    };
+    $scope.myChart = chart1;
+
+
+};
+
+
+
+
+
+
+  //  angular.module('TodoApp', ['googlechart'])
+  //.controller('myController', function ($scope) {
+  //    var chart1 = {};
+  //    chart1.type = "google.charts.Bar";
+  //    chart1.displayed = false;
+  //    chart1.data = {
+  //        "cols": [{
+  //            id: "month",
+  //            label: "Month",
+  //            type: "string"
+  //        }, {
+  //            id: "laptop-id",
+  //            label: "Laptop",
+  //            type: "number"
+  //        }, {
+  //            id: "desktop-id",
+  //            label: "Desktop",
+  //            type: "number"
+  //        }, {
+  //            id: "server-id",
+  //            label: "Server",
+  //            type: "number"
+  //        }, {
+  //            id: "cost-id",
+  //            label: "Shipping",
+  //            type: "number"
+  //        }],
+  //        "rows": [{
+  //            c: [{
+  //                v: "January"
+  //            }, {
+  //                v: 19,
+  //                f: "42 items"
+  //            }, {
+  //                v: 12,
+  //                f: "Ony 12 items"
+  //            }, {
+  //                v: 7,
+  //                f: "7 servers"
+  //            }, {
+  //                v: 4
+  //            }]
+  //        }, {
+  //            c: [{
+  //                v: "February"
+  //            }, {
+  //                v: 13
+  //            }, {
+  //                v: 1,
+  //                f: "1 unit (Out of stock this month)"
+  //            }, {
+  //                v: 12
+  //            }, {
+  //                v: 2
+  //            }]
+  //        }, {
+  //            c: [{
+  //                v: "March"
+  //            }, {
+  //                v: 24
+  //            }, {
+  //                v: 5
+  //            }, {
+  //                v: 11
+  //            }, {
+  //                v: 6
+  //            }
+
+  //            ]
+  //        }]
+  //    };
+
+  //    chart1.options = {
+  //        "title": "Sales per month",
+  //        "isStacked": "true",
+  //        "fill": 20,
+  //        "displayExactValues": true,
+  //        "vAxis": {
+  //            "title": "Sales unit",
+  //            "gridlines": {
+  //                "count": 10
+  //            }
+  //        },
+  //        "hAxis": {
+  //            "title": "Date"
+  //        }
+  //    };
+  //    $scope.myChart = chart1;
+  //})
+
+  //      .value('googleChartApiConfig', {
+  //    version: '1.1',
+  //    optionalSettings: {
+  //        packages: ['bar'],
+  //        language: 'en'
+  //    }
+  //});
 
